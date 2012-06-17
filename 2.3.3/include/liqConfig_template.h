f9f3e0370f4d08728097d8184bdf5b8fbda0b485
yaoyansi <yaoyansi2@yahoo.com.cn>
2012-06-17 12:01:43 +0800

    liq|: add macro for each renderer

--- 2.3.3/include/liqConfig_template.h
+++ 2.3.3/include/liqConfig_template.h
@@ -2,13 +2,15 @@
 #ifndef _LIQ_CONFIG_H_
 #define _LIQ_CONFIG_H_
 
+
 #define _USE_SHAVE_ 1
 
-#define _USE_RENDERMAN_
+//macros for renderers, if you use a renderer, define a macro for that renderer.
+//#define _USE_RENDERMAN_
 
-#define _USE_ELVISHRAY_
+//#define _USE_ELVISHRAY_
 
-#define _USE_APPLESEED_
+//#define _USE_APPLESEED_
 
 #define _USE_NEWRENDERER_//for your new renderer
 
