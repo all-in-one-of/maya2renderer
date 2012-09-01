d7f57e2467d671d40ebe77fab90cc465185e6435
yaoyansi <yaoyansi2@yahoo.com.cn>
2012-09-01 16:52:54 +0800

    liq|: use LIF file for er and as

---
+++ 2.3.3/test/test_shader_pl/myLifTest.pl
@@ -0,0 +1,10 @@
+#shaderType
+surface
+#shaderName
+myLifTest0
+#Name|Details|Type|IsOutput|Defaults|ArraySizes
+Ks|uniform|float|0|0.5|-1
+Kd|uniform|float|0|0.5|-1
+Ka|uniform|float|0|1.0|-1
+roughness|uniform|float|0|0.1|-1
+specularcolor|uniform|color|0|<<1.0,1.0,1.0>>|-1
