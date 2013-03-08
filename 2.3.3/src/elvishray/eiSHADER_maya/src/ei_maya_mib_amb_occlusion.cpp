9607846f96a4c2880dfaf6a65dd9d47900359b74
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-03-08 12:53:20 +0800

    liq| er| shader|: mib_amb_occlusion - adapted to new ER API

--- 2.3.3/src/elvishray/eiSHADER_maya/src/ei_maya_mib_amb_occlusion.cpp
+++ 2.3.3/src/elvishray/eiSHADER_maya/src/ei_maya_mib_amb_occlusion.cpp
@@ -18,30 +18,31 @@
 #include "common/_3delight/utils.h"
 
 SURFACE(maya_mib_amb_occlusion)
-	//input
-	PARAM(eiIndex,	samples);
-	PARAM(color,	bright);
-	PARAM(color,	dark);
-	PARAM(scalar,	spread);
-	PARAM(scalar,	max_distance);
-	PARAM(eiBool,	reflective);
-	//output
-	PARAM(color,	outValue);
+	DECLARE;
+	DECLARE_INDEX(	samples,			16); 
+	DECLARE_COLOR(	bright,				1.0f, 1.0f, 1.0f); 
+	DECLARE_COLOR(	dark,				0.0f, 0.0f, 0.0f); 
+	DECLARE_SCALAR(	spread,				0.8f); 
+	DECLARE_SCALAR(	max_distance,		0.0f);
+	DECLARE_BOOL(	reflective,			eiFALSE); 
+	DECLARE_COLOR(	outValue,			0.0f, 0.0f, 0.0f);
+	END_DECLARE;
 
-	void parameters(int pid)
+	static void init()
 	{
-		//input
-		DECLARE_INDEX(	samples,			16); 
-		DECLARE_COLOR(	bright,				1.0f, 1.0f, 1.0f); 
-		DECLARE_COLOR(	dark,				0.0f, 0.0f, 0.0f); 
-		DECLARE_SCALAR(	spread,				0.8f); 
-		DECLARE_SCALAR(	max_distance,		0.0f);
-		DECLARE_BOOL(	reflective,			eiFALSE); 
-		//output
-		DECLARE_COLOR(	outValue,			0.0f, 0.0f, 0.0f);
 	}
-	void init()	{}
-	void exit()	{}
+
+	static void exit()
+	{
+	}
+
+	void init_node()
+	{
+	}
+
+	void exit_node()
+	{
+	}
 
 	scalar occlusion(const point& p, const normal& N,
 		const eiIndex samples, 
@@ -50,12 +51,12 @@ SURFACE(maya_mib_amb_occlusion)
 	{
 		uint num_rays = max<eiIndex>(samples, 1);
 		int num_hit = 0;
-		geoscalar u[2];
-		uint count = 0;
 
-		while (sample(u, &count, 2, &num_rays))
+		UniformSampler<2> sampler(this, num_rays);
+
+		while (sampler.sample())
 		{
-			vector dir = sample_cosine_hemisphere(point(0.0f), N, 1.0f, (scalar)u[0], (scalar)u[1]);
+			vector dir = sample_cosine_hemisphere(point(0.0f), N, 1.0f, sampler[0], sampler[1]);
 			dir = normalize((1.0f - spread) * N + spread * dir);
 
 			if ( trace_probe(p, dir, max_distance ) )
@@ -72,17 +73,17 @@ SURFACE(maya_mib_amb_occlusion)
 		if(max_distance() < eiSCALAR_EPS)//If it is zero, the entire scene is sampled
 			max_distance() = eiMAX_SCALAR;
 
-		normal Nf = faceforward(normalize(N()), I());
+		normal Nf = faceforward(normalize(N), I);
 
 		uint num_rays = max<int>(samples(), 1);
 		int num_misses = 0;
-		geoscalar u[2];
-		uint count = 0;
 
-		while (sample(u, &count, 2, &num_rays))
+		UniformSampler<2> sampler(this, num_rays);
+
+		while (sampler.sample())
 		{
-			vector dir = sample_cosine_hemisphere(point(0.0f), Nf, 1.0f, (scalar)u[0], (scalar)u[1]);
-			if (!trace_probe(P(), dir, max_distance()))
+			vector dir = sample_cosine_hemisphere(point(0.0f), Nf, 1.0f, sampler[0], sampler[1]);
+			if (!trace_probe(P, dir, max_distance()))
 			{
 				++ num_misses;
 			}
@@ -91,8 +92,8 @@ SURFACE(maya_mib_amb_occlusion)
 		outValue() = color( (scalar)num_misses / (scalar)num_rays );
 		
 		//
-		Ci() = outValue();
-		Oi() = color(1.0f);
+		out->Ci = outValue();
+		out->Oi = color(1.0f);
 
 	}
 	void main_3delight(void *arg)
@@ -100,13 +101,13 @@ SURFACE(maya_mib_amb_occlusion)
 		if(max_distance() < eiSCALAR_EPS)//If it is zero, the entire scene is sampled
 			max_distance() = eiMAX_SCALAR;
 
-		normal Nf = faceforward(normalize(N()), I());
+		normal Nf = faceforward(normalize(N), I);
 		normal Nn = normalize(Nf);
 
 		if(reflective() != eiFALSE)
-			Nn = reflect( -I(), Nn );
+			Nn = reflect( -I, Nn );
 
-		const scalar occ = 1.0f - occlusion(P(), Nn, 
+		const scalar occ = 1.0f - occlusion(P, Nn, 
 									samples(),
 									max_distance(),
 									spread()
@@ -114,8 +115,8 @@ SURFACE(maya_mib_amb_occlusion)
 
 		outValue() = mix( dark(), bright(), occ );
 		//
-		Ci() = outValue();
-		Oi() = color(1.0f);
+		out->Ci = outValue();
+		out->Oi = color(1.0f);
 	}
 
 	void main(void *arg)
