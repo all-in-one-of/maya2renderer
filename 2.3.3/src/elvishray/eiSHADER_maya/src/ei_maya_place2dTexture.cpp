1f9fb82771b93408bfd9ab6b5b5a9f9ccd18b542
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-03-13 12:46:07 +0800

    liq| er| shader|: place2dTexture - adapted to new ER API

--- 2.3.3/src/elvishray/eiSHADER_maya/src/ei_maya_place2dTexture.cpp
+++ 2.3.3/src/elvishray/eiSHADER_maya/src/ei_maya_place2dTexture.cpp
@@ -3,75 +3,59 @@
 #include "common/_3delight/utils.h"
 
 SURFACE(maya_place2dTexture)
-	// Inputs
-	//PARAM(vector, uvCoord);//float2. //elvishray generates uv internally.
-	PARAM(scalar, coverageU);
-	PARAM(scalar, coverageV);
-	PARAM(eiBool, mirrorU);
-	PARAM(eiBool, mirrorV);
-	PARAM(scalar, noiseU);
-	PARAM(scalar, noiseV);
-	PARAM(scalar, offsetU);
-	PARAM(scalar, offsetV);
-	PARAM(scalar, repeatU);
-	PARAM(scalar, repeatV);
-	PARAM(scalar, rotateFrame);//angle(deg)
-	PARAM(scalar, rotateUV);//angle(deg)
-	PARAM(eiBool, stagger);
-	PARAM(scalar, translateFrameU);
-	PARAM(scalar, translateFrameV);
-	PARAM(eiBool, wrapU);
-	PARAM(eiBool, wrapV);
-	// Outputs
-	PARAM(vector, outUV);//float2
+	DECLARE;
+	DECLARE_SCALAR(coverageU, 1.0f);
+	DECLARE_SCALAR(coverageV, 1.0f);
+	DECLARE_BOOL(mirrorU, eiFALSE);
+	DECLARE_BOOL(mirrorV, eiFALSE);
+	DECLARE_SCALAR(noiseU, 0.0f);
+	DECLARE_SCALAR(noiseV, 0.0f);
+	DECLARE_SCALAR(offsetU, 0.0f);
+	DECLARE_SCALAR(offsetV, 0.0f);
+	DECLARE_SCALAR(repeatU, 1.0f);
+	DECLARE_SCALAR(repeatV, 1.0f);
+	DECLARE_SCALAR(rotateFrame, 0.0f);//angle(deg)
+	DECLARE_SCALAR(rotateUV, 0.0f);//angle(deg)
+	DECLARE_BOOL(stagger, eiFALSE);
+	DECLARE_SCALAR(translateFrameU, 0.0f);
+	DECLARE_SCALAR(translateFrameV, 0.0f);
+	DECLARE_BOOL(wrapU, eiTRUE);
+	DECLARE_BOOL(wrapV, eiTRUE);
+	DECLARE_VECTOR(outUV,    0.0f, 0.0f, -1.0f); //float2
+	END_DECLARE;
+	//DECLARE_VECTOR(uvCoord, 0.0f, 0.0f, 0.0f);//float2, //elvishray generates uv internally.
 
-	void parameters(int pid)
-	{
-		// Inputs
-		//DECLARE_VECTOR(uvCoord, 0.0f, 0.0f, 0.0f);//float2
-		DECLARE_SCALAR(coverageU, 1.0f);
-		DECLARE_SCALAR(coverageV, 1.0f);
-		DECLARE_BOOL(mirrorU, eiFALSE);
-		DECLARE_BOOL(mirrorV, eiFALSE);
-		DECLARE_SCALAR(noiseU, 0.0f);
-		DECLARE_SCALAR(noiseV, 0.0f);
-		DECLARE_SCALAR(offsetU, 0.0f);
-		DECLARE_SCALAR(offsetV, 0.0f);
-		DECLARE_SCALAR(repeatU, 1.0f);
-		DECLARE_SCALAR(repeatV, 1.0f);
-		DECLARE_SCALAR(rotateFrame, 0.0f);//angle(deg)
-		DECLARE_SCALAR(rotateUV, 0.0f);//angle(deg)
-		DECLARE_BOOL(stagger, eiFALSE);
-		DECLARE_SCALAR(translateFrameU, 0.0f);
-		DECLARE_SCALAR(translateFrameV, 0.0f);
-		DECLARE_BOOL(wrapU, eiTRUE);
-		DECLARE_BOOL(wrapV, eiTRUE);
-		// Outputs
-		DECLARE_VECTOR(outUV,    0.0f, 0.0f, -1.0f); //float2
-	}
-
-	void init()
+	static void init()
 	{
 	}
 
-	void exit()
+	static void exit()
 	{
 	}
+
+	void init_node()
+	{
+	}
+
+	void exit_node()
+	{
+	}
+
 	void main(void *arg)
 	{
-		//main0(arg);
-		main1(arg);
+		main_er0(arg);
+		//main_3delight(arg);
 	}
-	void main0(void *arg)
+	void main_er0(void *arg)
 	{
-		outUV().x = fmodf( u() * repeatU(), 1.0f );
-		outUV().y = fmodf( v() * repeatV(), 1.0f );
-		outUV().y = 1.0f - outUV().y;//adjust v for elvishray
+		outUV().x = fmodf( u * repeatU(), 1.0f );
+		outUV().y = fmodf( v * repeatV(), 1.0f );
+ 		outUV().y = 1.0f - outUV().y;//adjust v for elvishray
 	}
-	void main1(void *arg)
+	void main_3delight(void *arg)
 	{
-		float ss = u();
-		float tt = 1.0f - v();//adjust v for elvishray
+		float ss = u;
+		float tt = 1.0f - v;//adjust v for elvishray
 
 		float outU = ss;
 		float outV = tt;
