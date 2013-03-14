79338f1a249d2a0062ab7e87cda1fe6b0cea7c69
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-03-14 20:55:18 +0800

    liq| er|: test skylight, (crash, due to the "env_shaer" is null)

---
+++ 2.3.3/test/test_er_light/skylight.pl
@@ -0,0 +1,8 @@
+#shaderType
+light
+#shaderName
+skylight
+#Name|Details|Type|IsOutput|Defaults|ArraySizes
+#env_shader|uniform|tag|0|?|-1 
+resolution|uniform|int|0|1000|-1 
+max_dist|uniform|float|0|10000.0|-1
