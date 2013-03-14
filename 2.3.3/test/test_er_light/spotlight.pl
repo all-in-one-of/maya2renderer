29485efe767aea60f7e258e14be4cd988930082e
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-03-14 20:30:35 +0800

    liq| er|: spotlight.pl, update default values of parameters

--- 2.3.3/test/test_er_light/spotlight.pl
+++ 2.3.3/test/test_er_light/spotlight.pl
@@ -1,10 +1,12 @@
 #shaderType
-surface
+light
 #shaderName
-architectural
+spotlight
 #Name|Details|Type|IsOutput|Defaults|ArraySizes
 intensity|uniform|float|0|1.0|-1
 lightcolor|uniform|color|0|<<1.000000,1.000000,1.000000>>|-1
-deltaangle|uniform|float|0|0.6981317007977318|-1
+#deltaangle = 10 degree
+deltaangle|uniform|float|0|0.1745329251994329|-1
 direction|uniform|vector|0|<<0.000000,0.000000,-1.000000>>|-1
-spread|uniform|float|0|0.0|-1
+#spread = 20 degree
+spread|uniform|float|0|0.3490658503988659|-1
