e74d600383c23b86bf3acb2aace00a1a24ac9c60
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-03-14 20:41:49 +0800

    liq| er|: test spherelight

---
+++ 2.3.3/test/test_er_light/spherelight.pl
@@ -0,0 +1,8 @@
+#shaderType
+light
+#shaderName
+spherelight
+#Name|Details|Type|IsOutput|Defaults|ArraySizes
+intensity|uniform|float|0|1.0|-1
+lightcolor|uniform|color|0|<<1.000000,1.000000,1.000000>>|-1
+radius|uniform|float|0|1.0|-1
