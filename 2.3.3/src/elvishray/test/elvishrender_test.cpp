373eead5e0a6ab95b453714106c1eb67581736f3
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-03-16 21:44:51 +0800

    liq| rm| stereocamera|: update test case for 3delight

--- 2.3.3/src/elvishray/test/elvishrender_test.cpp
+++ 2.3.3/src/elvishray/test/elvishrender_test.cpp
@@ -62,7 +62,7 @@ void test_liquid2er()
 	//ei_shader_link_test(false, "suffix");
 	//ei_checker_uv();
 	//er_shader_transparent_test();
-#include "E:/MyDocuments/maya/projects/default/rib/test_procedural.perspShape.1.elvishray.bmp.er"
+#include "E:/MyDocuments/maya/projects/default/rib/er_aov_perspShape_1_elvishray.tif.er"
 
 	//ei_displace_unittest();
 	//ei_volume_unittest();
