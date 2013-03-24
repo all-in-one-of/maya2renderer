e91d3f7e27396ceda280fd6999c78bb67f455f6b
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-03-24 23:56:56 +0800

    liq| test| er|: quard light (liquidShaderNode)

--- 2.3.3/test/test_er_light/quadlight.pl
+++ 2.3.3/test/test_er_light/quadlight.pl
@@ -5,10 +5,10 @@ quadlight
 #Name|Details|Type|IsOutput|Defaults|ArraySizes
 intensity|uniform|float|0|1.0|-1
 lightcolor|uniform|color|0|<<1.000000,1.000000,1.000000>>|-1
-#deltaangle = 10 degree
-deltaangle|uniform|float|0|0.1745329251994329|-1
+#deltaangle = eiPI / 2.0
+deltaangle|uniform|float|0|1.5708|-1
 direction|uniform|vector|0|<<0.000000,0.000000,-1.000000>>|-1
-#spread = 20 degree
-spread|uniform|float|0|0.3490658503988659|-1
-width|uniform|float|0|20.0|-1
-height|uniform|float|0|20.0|-1
+#spread = eiPI / 4.0f
+spread|uniform|float|0|0.785398|-1
+width|uniform|float|0|1.0|-1
+height|uniform|float|0|1.0|-1
