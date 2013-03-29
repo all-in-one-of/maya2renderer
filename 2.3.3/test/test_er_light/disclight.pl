35cf1d64ec41caecbf6f8571f3e3578922cf0fd5
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-03-29 11:03:51 +0800

    liq| er| light|: + disclight

---
+++ 2.3.3/test/test_er_light/disclight.pl
@@ -0,0 +1,13 @@
+#shaderType
+light
+#shaderName
+disclight
+#Name|Details|Type|IsOutput|Defaults|ArraySizes
+intensity|uniform|float|0|1.0|-1
+lightcolor|uniform|color|0|<<1.000000,1.000000,1.000000>>|-1
+#deltaangle = 10 degree
+deltaangle|uniform|float|0|0.1745329251994329|-1
+direction|uniform|vector|0|<<0.000000,0.000000,-1.000000>>|-1
+#spread = 20 degree
+spread|uniform|float|0|0.3490658503988659|-1
+radius|uniform|float|0|10.0|-1
