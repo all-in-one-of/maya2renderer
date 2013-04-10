96655b61da25d1cfed0c78f6e2e02cc3dde2d771
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-04-10 23:21:42 +0800

    liq| er|: support locator data

--- 2.3.3/src/elvishray/er_writeLocatorData.cpp
+++ 2.3.3/src/elvishray/er_writeLocatorData.cpp
@@ -42,8 +42,38 @@ namespace elvishray
 	{
 		CM_TRACE_FUNC("er_writeLocatorData.cpp::write("<<pData->getFullPathName()<<","<<currentJob__.name.asChar()<<",...)");
 
-		_s("//locator "<<pData->getFullPathName()<<" is not implemented yet.");
-		liquidMessage2(messageError, "Locator type is not implemented yet.\n%s", pData->getFullPathName());
+		_s("\n\n\n//locator "<<pData->getFullPathName());
+
+#ifdef TRANSFORM_SHAPE_PAIR
+		const std::string objectName(pData->getFullPathName());//shape
+#else// SHAPE SHAPE_object PAIR
+		const std::string objectName(getObjectName(pData->getFullPathName()));//shape+"_object"
+#endif
+		_S( ei_object( "poly", objectName.c_str() ) );
+		_s("{");
+		_d( eiTag tag );
+
+		_s("//vertex positions" );
+		_d( tag = ei_tab(EI_TYPE_VECTOR, 1024) )
+		_S( ei_pos_list( tag ) );
+		_s("// empty list" );
+		_S( ei_end_tab() );
+
+// 		_s("//uv" );
+// 		_d( tag = eiNULL_TAG );
+// 		_S( ei_declare("uv", EI_VARYING, EI_TYPE_TAG, &tag) );
+// 		_d( tag = ei_tab(EI_TYPE_VECTOR2, 1024) )
+// 		_S( ei_variable("uv", &tag) );
+// 		_s("// empty list" );
+// 		_S( ei_end_tab() );
+
+		_s("//triangles");
+		_d( tag = ei_tab(EI_TYPE_INDEX, 1024) )
+		_S( ei_triangle_list( tag ) );
+		_s("// empty list" );
+		_S( ei_end_tab() );
+		_s("}//"<<objectName);
+		_S( ei_end_object() );
 	}
 }//namespace elvishray
 #endif//_USE_ELVISHRAY_
