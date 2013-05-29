d7aeae9ab9dc53932fba9437ea3ad3b7373c24f1
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-05-29 16:19:14 +0800

    liq|: a script which converts images to video with ffmpeg

---
+++ test/ffmpeg_cmd.bat
@@ -0,0 +1,3 @@
+ffmpeg.exe -r 10 -i "G:/tmp/d_tmp/maya_test/tutorial/Transforming.Robot/er_render/robot_anim_1024x768/images/05_begin_liq_noIBL_03_truck_tex_noNurbs_nolight_specular__7128.shotCamShape.elvishray.%04d.bmp" -c:v libx264 -qscale 1 -pix_fmt yuv420p G:/tmp/d_tmp/maya_test/tutorial/Transforming.Robot/er_render/robot_anim_1024x768/robot_anim_1024x768_fps10.mp4
+
+pause
