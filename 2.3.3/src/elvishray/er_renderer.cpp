1f077d75bffb2d4b63535a2164abccb28737f41d
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-06-01 00:29:56 +0800

    liq| er| check|: check fabs(transform.scale)!=0 to avoid the degenerated mesh. Not show the mel command.

--- 2.3.3/src/elvishray/er_renderer.cpp
+++ 2.3.3/src/elvishray/er_renderer.cpp
@@ -1281,11 +1281,11 @@ namespace elvishray
 		//-------------------------------------
 		//check blinn's eccentricity > 0
 		MStringArray blinns;
-		MGlobal::executeCommand("ls -type blinn", blinns, true);
+		IfMErrorWarn(MGlobal::executeCommand("ls -type blinn", blinns));
 		for(std::size_t i=0; i<blinns.length(); ++i)
 		{
 			double eccentricity;
-			MGlobal::executeCommand("getAttr "+blinns[i]+".eccentricity", eccentricity, true);
+			IfMErrorWarn(MGlobal::executeCommand("getAttr "+blinns[i]+".eccentricity", eccentricity));
 			if( eccentricity < LIQ_SCALAR_ALMOST_ZERO )
 			{
 				liquidMessage2(messageError,"blinn.eccentricity should not be 0, or the maya_blinn shader may generate black points. [%s]", blinns[i].asChar());
@@ -1295,23 +1295,23 @@ namespace elvishray
 		//-------------------------------------
 		//transform scalar >0
 		MStringArray transforms;
-		MGlobal::executeCommand("ls -type transform", transforms, true);
+		IfMErrorWarn(MGlobal::executeCommand("ls -type transform", transforms));
 		for(std::size_t i=0; i<transforms.length(); ++i)
 		{
 			MStringArray srcNodes;
 			MVector scale;
-
-			MGlobal::executeCommand("listConnections -s true -shapes false "+transforms[i]+".scale", srcNodes, true);
+			0
+			IfMErrorWarn(MGlobal::executeCommand("listConnections -s true -shapes false "+transforms[i]+".scale", srcNodes));
 			if( srcNodes.length() >0 )//scale is connected in, I ignore this case
 			{
 				continue;
 			}
 			
 			//scaleX
-			MGlobal::executeCommand("listConnections -s true -shapes false "+transforms[i]+".scaleX", srcNodes, true);
+			IfMErrorWarn(MGlobal::executeCommand("listConnections -s true -shapes false "+transforms[i]+".scaleX", srcNodes));
 			if( srcNodes.length() == 0 )
 			{
-				MGlobal::executeCommand("getAttr "+transforms[i]+".scaleX", scale.x, true);
+				IfMErrorWarn(MGlobal::executeCommand("getAttr "+transforms[i]+".scaleX", scale.x));
 				if( fabs(scale.x) < LIQ_SCALAR_ALMOST_ZERO )
 				{
 					liquidMessage2(messageError,"transfrom.scaleX should not be 0, or degenerated polygon will occur. [%s]", transforms[i].asChar());
@@ -1319,10 +1319,10 @@ namespace elvishray
 				}
 			}
 			//scaleY
-			MGlobal::executeCommand("listConnections -s true -shapes false "+transforms[i]+".scaleY", srcNodes, true);
+			IfMErrorWarn(MGlobal::executeCommand("listConnections -s true -shapes false "+transforms[i]+".scaleY", srcNodes));
 			if( srcNodes.length() == 0 )
 			{
-				MGlobal::executeCommand("getAttr "+transforms[i]+".scaleY", scale.y, true);
+				IfMErrorWarn(MGlobal::executeCommand("getAttr "+transforms[i]+".scaleY", scale.y));
 				if( fabs(scale.y) < LIQ_SCALAR_ALMOST_ZERO )
 				{
 					liquidMessage2(messageError,"transfrom.scaleY should not be 0, or degenerated polygon will occur. [%s]", transforms[i].asChar());
@@ -1330,10 +1330,10 @@ namespace elvishray
 				}
 			}
 			//scaleZ
-			MGlobal::executeCommand("listConnections -s true -shapes false "+transforms[i]+".scaleZ", srcNodes, true);
+			IfMErrorWarn(MGlobal::executeCommand("listConnections -s true -shapes false "+transforms[i]+".scaleZ", srcNodes));
 			if( srcNodes.length() == 0 )
 			{
-				MGlobal::executeCommand("getAttr "+transforms[i]+".scaleZ", scale.z, true);
+				IfMErrorWarn(MGlobal::executeCommand("getAttr "+transforms[i]+".scaleZ", scale.z));
 				if( fabs(scale.z) < LIQ_SCALAR_ALMOST_ZERO )
 				{
 					liquidMessage2(messageError,"transfrom.scaleZ should not be 0, or degenerated polygon will occur. [%s]", transforms[i].asChar());
