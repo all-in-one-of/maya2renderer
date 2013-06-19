66fca17793c4c0f19b31aa5fb5e0faee1e4bbeda
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-06-19 19:02:48 +0800

    liq| er|: omit unused code: Renderer::framePrologue_camera()

--- 2.3.3/src/elvishray/er_renderer.cpp
+++ 2.3.3/src/elvishray/er_renderer.cpp
@@ -753,107 +753,108 @@ namespace elvishray
 	}
 	MStatus Renderer::framePrologue_camera(long lframe, const structJob &currentJob)
 	{
-		CM_TRACE_FUNC("Renderer::framePrologue_camera("<<lframe<<","<<currentJob.name.asChar()<<")");
-
-		MStatus status;
-		MFnCamera fnCamera(currentJob.path, &status);
-		IfMErrorWarn(status);
-
-		//
-		float focal;
-		float aperture;
-		float aspect;
-		float width=currentJob.width, height=currentJob.height;
-
-// 		// use liquid settings
-// 		focal = currentJob.camera[0].focalLength;
-// 		aperture = 2.0 * focal * tan(currentJob.camera[0].hFOV/2.0);
-// 		aspect = currentJob.aspectRatio;
-// 		_s("// liquid settings: focal="<<focal <<", aperture = "<<aperture<<", aspect="<<aspect );
-// 		//e.g. focal=3.5, aperture = 2.7, aspect=1
-		// use maya settings
-		//NOTE: we will caculate these setting from structJob data.[yaoyansi]
-		focal = fnCamera.focalLength(&status);
-		IfMErrorWarn(status);
-		double horizontalFOV,verticalFOV;
-		fnCamera.getPortFieldOfView(width, height, horizontalFOV,verticalFOV);
-		aperture = 2.0f * focal * tan(horizontalFOV /2.0f);
-		aspect = aperture / (2.0f * focal * tan(verticalFOV / 2.0f));
-		o.a(boost::str(boost::format(" maya settings: focal=%f, aperture = %f, aspect=%f")%focal %aperture %aspect ));
-		//e.g. focal=35, aperture = 36, aspect=1.33333.
-
-		//get camera transform matrix
-		//currentJob.camera[0].mat.get( m ) );
-		MObject transformNode = currentJob.path.transform(&status );
-		IfMErrorWarn(status);
-		MFnDagNode transform (transformNode, &status);
-		IfMErrorWarn(status);
-		MTransformationMatrix   m0(transform.transformationMatrix());
-		liqMatrix m;		
-		IfMErrorWarn(m0.asMatrix().get(m));
-
-		bool bDepthOfField;//enable DOF on this camera?
-		liquidGetPlugValue(fnCamera,"depthOfField", bDepthOfField, status);
-		o.a(boost::str(boost::format("Depth of Field on camera \"%s\" is turned %s in Maya") %currentJob.camera[0].name.asChar() %(bDepthOfField?"on":"off")));
-		bDepthOfField = bDepthOfField && liqglo.doDof && currentJob.pass != rpShadowMap;
-
-		MStringArray LensShaders, EnvironmentShaders;
-		{
-			//lens shader
-			if(bDepthOfField)
-			{
-				gatherCameraShaders(LensShaders, currentJob.camera[0].name, "liqLensShader");
-
-			}
-			//env shader
-			gatherCameraShaders(EnvironmentShaders, currentJob.camera[0].name, "liqEnvironmentShader");
-		}
-
-		o.ln();
-		o.a("############################### camera #");
-		std::string sCameraObjectName(std::string(currentJob.camera[0].name.asChar())+"_object");
-		o.ei_camera( sCameraObjectName.c_str() );
-			//_S( ei_frame( lframe, off ) );
-
-			cameraOutput(currentJob);
-
-			o.ei_focal( focal );
-			o.ei_aperture( aperture / fnCamera.overscan() );
-			o.ei_aspect( aspect );
-			if( currentJob.pass != rpShadowMap && liqglo.liqglo_rotateCamera  == true ) {
-				o.ei_resolution(height, width);
-			}else{ 
-				o.ei_resolution(width, height);
-			}
-
-			if( liqglo.m_renderViewCrop )
-			{
-				unsigned int left, right, bottom, top;
-				MRenderView::getRenderRegion(left, right, bottom, top);
-				o.ei_window(left, right+1, height-top, height-bottom+1);
-			} 
-
-			o.ei_clip( currentJob.camera[0].neardb, currentJob.camera[0].fardb);
-			
-			//lens shader
-			for( std::size_t i = 0; i<LensShaders.length(); ++i)
-			{
-				o.ei_lens_shader( LensShaders[i].asChar() );
-			}
-			//env shader
-			for( std::size_t i = 0; i<EnvironmentShaders.length(); ++i)
-			{
-				o.ei_environment_shader(EnvironmentShaders[i].asChar());
-			}
-		o.ei_end_camera();
-		o.a("----------------------------------");
-		o.ei_instance( currentJob.camera[0].name.asChar()	);
-			o.ei_element(	sCameraObjectName.c_str() );
-			o.ei_transform( m[0][0], m[0][1], m[0][2], m[0][3],   m[1][0], m[1][1], m[1][2], m[1][3],   m[2][0], m[2][1], m[2][2], m[2][3],   m[3][0], m[3][1], m[3][2], m[3][3] );
-// 			o.ei_motion( bMotionBlur || bGeometryMotion );
-		o.ei_end_instance();
-		o.ln();		
-		m_groupMgr->addObjectInstance(currentJob.name.asChar(), currentJob.camera[0].name.asChar(), GIT_Camera);//_S( ei_init_instance( currentJob.camera[0].name.asChar() ) );
+		//NOTE: this function is moved to er_writeCameraData.cpp
+//		CM_TRACE_FUNC("Renderer::framePrologue_camera("<<lframe<<","<<currentJob.name.asChar()<<")");
+//
+//		MStatus status;
+//		MFnCamera fnCamera(currentJob.path, &status);
+//		IfMErrorWarn(status);
+//
+//		//
+//		float focal;
+//		float aperture;
+//		float aspect;
+//		float width=currentJob.width, height=currentJob.height;
+//
+//// 		// use liquid settings
+//// 		focal = currentJob.camera[0].focalLength;
+//// 		aperture = 2.0 * focal * tan(currentJob.camera[0].hFOV/2.0);
+//// 		aspect = currentJob.aspectRatio;
+//// 		_s("// liquid settings: focal="<<focal <<", aperture = "<<aperture<<", aspect="<<aspect );
+//// 		//e.g. focal=3.5, aperture = 2.7, aspect=1
+//		// use maya settings
+//		//NOTE: we will caculate these setting from structJob data.[yaoyansi]
+//		focal = fnCamera.focalLength(&status);
+//		IfMErrorWarn(status);
+//		double horizontalFOV,verticalFOV;
+//		fnCamera.getPortFieldOfView(width, height, horizontalFOV,verticalFOV);
+//		aperture = 2.0f * focal * tan(horizontalFOV /2.0f);
+//		aspect = aperture / (2.0f * focal * tan(verticalFOV / 2.0f));
+//		o.a(boost::str(boost::format(" maya settings: focal=%f, aperture = %f, aspect=%f")%focal %aperture %aspect ));
+//		//e.g. focal=35, aperture = 36, aspect=1.33333.
+//
+//		//get camera transform matrix
+//		//currentJob.camera[0].mat.get( m ) );
+//		MObject transformNode = currentJob.path.transform(&status );
+//		IfMErrorWarn(status);
+//		MFnDagNode transform (transformNode, &status);
+//		IfMErrorWarn(status);
+//		MTransformationMatrix   m0(transform.transformationMatrix());
+//		liqMatrix m;		
+//		IfMErrorWarn(m0.asMatrix().get(m));
+//
+//		bool bDepthOfField;//enable DOF on this camera?
+//		liquidGetPlugValue(fnCamera,"depthOfField", bDepthOfField, status);
+//		o.a(boost::str(boost::format("Depth of Field on camera \"%s\" is turned %s in Maya") %currentJob.camera[0].name.asChar() %(bDepthOfField?"on":"off")));
+//		bDepthOfField = bDepthOfField && liqglo.doDof && currentJob.pass != rpShadowMap;
+//
+//		MStringArray LensShaders, EnvironmentShaders;
+//		{
+//			//lens shader
+//			if(bDepthOfField)
+//			{
+//				gatherCameraShaders(LensShaders, currentJob.camera[0].name, "liqLensShader");
+//
+//			}
+//			//env shader
+//			gatherCameraShaders(EnvironmentShaders, currentJob.camera[0].name, "liqEnvironmentShader");
+//		}
+//
+//		o.ln();
+//		o.a("############################### camera #");
+//		std::string sCameraObjectName(std::string(currentJob.camera[0].name.asChar())+"_object");
+//		o.ei_camera( sCameraObjectName.c_str() );
+//			//_S( ei_frame( lframe, off ) );
+//
+//			cameraOutput(currentJob);
+//
+//			o.ei_focal( focal );
+//			o.ei_aperture( aperture / fnCamera.overscan() );
+//			o.ei_aspect( aspect );
+//			if( currentJob.pass != rpShadowMap && liqglo.liqglo_rotateCamera  == true ) {
+//				o.ei_resolution(height, width);
+//			}else{ 
+//				o.ei_resolution(width, height);
+//			}
+//
+//			if( liqglo.m_renderViewCrop )
+//			{
+//				unsigned int left, right, bottom, top;
+//				MRenderView::getRenderRegion(left, right, bottom, top);
+//				o.ei_window(left, right+1, height-top, height-bottom+1);
+//			} 
+//
+//			o.ei_clip( currentJob.camera[0].neardb, currentJob.camera[0].fardb);
+//			
+//			//lens shader
+//			for( std::size_t i = 0; i<LensShaders.length(); ++i)
+//			{
+//				o.ei_lens_shader( LensShaders[i].asChar() );
+//			}
+//			//env shader
+//			for( std::size_t i = 0; i<EnvironmentShaders.length(); ++i)
+//			{
+//				o.ei_environment_shader(EnvironmentShaders[i].asChar());
+//			}
+//		o.ei_end_camera();
+//		o.a("----------------------------------");
+//		o.ei_instance( currentJob.camera[0].name.asChar()	);
+//			o.ei_element(	sCameraObjectName.c_str() );
+//			o.ei_transform( m[0][0], m[0][1], m[0][2], m[0][3],   m[1][0], m[1][1], m[1][2], m[1][3],   m[2][0], m[2][1], m[2][2], m[2][3],   m[3][0], m[3][1], m[3][2], m[3][3] );
+//// 			o.ei_motion( bMotionBlur || bGeometryMotion );
+//		o.ei_end_instance();
+//		o.ln();		
+//		m_groupMgr->addObjectInstance(currentJob.name.asChar(), currentJob.camera[0].name.asChar(), GIT_Camera);//_S( ei_init_instance( currentJob.camera[0].name.asChar() ) );
 
 		return MStatus::kSuccess;
 	}
