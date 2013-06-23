5d5fe7b0f1ac8e0fd26e6eb1bb2cad04d7ede2f5
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-06-23 23:43:14 +0800

    liq| er| eiSHADER_user|: 3. +eiSHADER_user_mayaplugin. forgotten file

---
+++ 2.3.3/src/elvishray/er_export.h
@@ -0,0 +1,19 @@
+#ifndef _ER_EXPORT_H_
+#define _ER_EXPORT_H_
+
+//
+#define ER_EXPORT_DLL
+//
+#ifdef _WIN32
+#	ifdef ER_EXPORT_DLL
+#		define ER_EXPORT _declspec(dllexport)
+#	else
+#		define ER_EXPORT _declspec(dllimport)
+#	endif
+#else
+#	define ER_EXPORT
+#endif
+//
+
+
+#endif//_ER_EXPORT_H_
