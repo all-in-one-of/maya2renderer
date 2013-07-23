0d4b8eff3580825f2b7ed7cf383382a21378e94e
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-07-23 14:38:43 +0800

    liq| er|: update er_maketexture: x64, shader parameters name(e.g. coverageU --> i_coverageU)

---
+++ 2.3.3/src/elvishray/test/er_maketexture/x64/Debug/run_er_maketexture.bat
@@ -0,0 +1,5 @@
+cd E:\dev\Autodesk\maya\myplugin\project\liquid_\dependence\elvishray\r274\bin_x64
+
+rem E:\dev\Autodesk\maya\myplugin\project\liquid_\2.3.3\src\elvishray\test\er_maketexture\x64\Debug\er_maketexture.exe      G:\tmp\d_tmp\maya_test\creativecrash\Chicken_Rig\1.0.0\Chicken_Rig\sourceimages\BodyBump.bmp
+E:\dev\Autodesk\maya\myplugin\project\liquid_\2.3.3\src\elvishray\test\er_maketexture\x64\Debug\er_maketexture.exe      E:\dev\Autodesk\maya\myplugin\project\liquid_\test\texture\check112.tif
+pause
