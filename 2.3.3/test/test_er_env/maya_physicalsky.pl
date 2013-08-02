d63bb6edf0b6d5b716e4111cd8a02718d5f8eca9
yaoyansi <yaoyansi2@yahoo.com.cn>
2013-08-03 01:05:47 +0800

    liq| er|: use ER_SPN() for parameters in liq_skylight, maya_physicalsky, update test case and .pl file

--- 2.3.3/test/test_er_env/maya_physicalsky.pl
+++ 2.3.3/test/test_er_env/maya_physicalsky.pl
@@ -3,20 +3,20 @@ environment
 #shaderName
 maya_physicalsky
 #Name|Details|Type|IsOutput|Defaults|ArraySizes
-sun_dir|uniform|vector|0|<<0.577, 0.577, 0.577>>|-1
-sun_disk_size|uniform|float|0|2.0|-1
-sun_disk_intensity|uniform|float|0|7.0|-1
-sun_glow_size|uniform|float|0|1.0|-1
-sun_glow_intensity|uniform|float|0|1.0|-1
-sun_glow_falloff|uniform|float|0|5.0|-1
-ground_color|uniform|color|0|<<0.200000,0.200000,0.200000>>|-1
-ground_blur|uniform|float|0|0.01|-1
-type|uniform|int|0|0|-1
-haze|uniform|float|0|5.0|-1
-zenith_color|uniform|color|0|<<0.10900,0.10900,0.10900>>|-1
-a|uniform|float|0|-1.0|-1
-b|uniform|float|0|-0.32|-1
-c|uniform|float|0|10.0|-1
-d|uniform|float|0|-3.0|-1
-e|uniform|float|0|0.45|-1
-intensity|uniform|float|0|0.5|-1
+i_sun_dir|uniform|vector|0|<<0.577, 0.577, 0.577>>|-1
+i_sun_disk_size|uniform|float|0|2.0|-1
+i_sun_disk_intensity|uniform|float|0|7.0|-1
+i_sun_glow_size|uniform|float|0|1.0|-1
+i_sun_glow_intensity|uniform|float|0|1.0|-1
+i_sun_glow_falloff|uniform|float|0|5.0|-1
+i_ground_color|uniform|color|0|<<0.200000,0.200000,0.200000>>|-1
+i_ground_blur|uniform|float|0|0.01|-1
+i_type|uniform|int|0|0|-1
+i_haze|uniform|float|0|5.0|-1
+i_zenith_color|uniform|color|0|<<0.10900,0.10900,0.10900>>|-1
+i_a|uniform|float|0|-1.0|-1
+i_b|uniform|float|0|-0.32|-1
+i_c|uniform|float|0|10.0|-1
+i_d|uniform|float|0|-3.0|-1
+i_e|uniform|float|0|0.45|-1
+i_intensity|uniform|float|0|0.5|-1
--- 2.3.3/test/test_er_light/liq_skylight.pl
+++ 2.3.3/test/test_er_light/liq_skylight.pl
@@ -3,6 +3,6 @@ light
 #shaderName
 liq_skylight
 #Name|Details|Type|IsOutput|Defaults|ArraySizes
-env_shader|uniform|shader|0|""|-1
-resolution|uniform|int|0|1000|-1
-max_dist|uniform|float|0|10000.0|-1
+i_env_shader|uniform|shader|0|""|-1
+i_resolution|uniform|int|0|1000|-1
+i_max_dist|uniform|float|0|10000.0|-1
